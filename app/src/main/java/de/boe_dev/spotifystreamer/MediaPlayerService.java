55dec26e6db62eec6ebf7bcc41312a1692ccb2d3
boe-dev <info@boe-dev.de>
2015-08-23 01:43:15 +0200

    seekbar works + features for headphones and incomming calles

--- app/src/main/java/de/boe_dev/spotifystreamer/MediaPlayerService.java
+++ app/src/main/java/de/boe_dev/spotifystreamer/MediaPlayerService.java
@@ -1,11 +1,16 @@
 package de.boe_dev.spotifystreamer;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import java.io.IOException;
@@ -22,16 +27,64 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
     private int pos;
     private boolean preparded = false;
     private static final int NOTIFICATION_ID = 13;
+    private boolean isPausedInCall = false;
 
     private MediaPlayer mediaPlayer;
     private final IBinder MediaPlayerBinder = new MyLocalBinder();
+    private PhoneStateListener phoneStateListener;
+    private TelephonyManager telephonyManager;
+
+    public static final String  BUFFER = "de.boe_dev.media_player_buffer";
+    private Intent bufferIntent;
+
+    private int headsetSwitch = 1;
 
 
     @Override
     public void onDestroy() {
         mediaPlayer.stop();
         mediaPlayer.release();
+        telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_NONE);
         super.onDestroy();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        bufferIntent = new Intent(BUFFER);
+        registerReceiver(headsetReceiver, new IntentFilter(Intent.ACTION_HEADSET_PLUG));
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        //This code should pause the music if a phone call comes in
+        telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        phoneStateListener = new PhoneStateListener() {
+            @Override
+            public void onCallStateChanged(int state, String incomingNumber) {
+
+                switch (state) {
+                    case TelephonyManager.CALL_STATE_RINGING:
+                        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                            pause();
+                            isPausedInCall = true;
+                        }
+                        break;
+
+                    case TelephonyManager.CALL_STATE_IDLE:
+                        if (mediaPlayer != null && isPausedInCall) {
+                            play();
+                        }
+
+                }
+
+                super.onCallStateChanged(state, incomingNumber);
+            }
+        };
+        telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -39,11 +92,36 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
 
         Log.d("MediaPlayerSerivce", "onBind");
         ItemDetailsWrapper wrap = (ItemDetailsWrapper) intent.getSerializableExtra("list");
-
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setOnPreparedListener(this);
+        mediaPlayer.setOnCompletionListener(this);
+        mediaPlayer.setOnSeekCompleteListener(this);
         return MediaPlayerBinder;
     }
+
+    private BroadcastReceiver headsetReceiver = new BroadcastReceiver() {
+
+        private boolean headsetConnect = false;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if (intent.hasExtra("state")) {
+                if (headsetConnect && intent.getIntExtra("state", 0) == 0) {
+                    headsetConnect = false;
+                    headsetSwitch = 0;
+                } else if (!headsetConnect && intent.getIntExtra("state", 0) == 1) {
+                    headsetConnect = true;
+                    headsetSwitch = 1;
+                }
+            }
+
+            if (headsetSwitch == 0) {
+                pause();
+            }
+
+        }
+    };
 
     public void setupPlayer(ArrayList<TopTrackModel> list, int pos) {
         this.list = list;
@@ -56,6 +134,7 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
     }
 
     public void pause() {
+        sendBufferingBroadcast("2");
         mediaPlayer.pause();
     }
 
@@ -93,7 +172,7 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
 //            mediaPlayer = null;
             mediaPlayer.reset();
         }
-
+        sendBufferingBroadcast("1");
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         try {
             mediaPlayer.setDataSource(list.get(pos).getPreviewUrl());
@@ -104,6 +183,14 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
         }
 
 
+    }
+
+    /**
+        1 is buffering, 0 is buffering is completed
+     */
+    private void sendBufferingBroadcast(String state) {
+        bufferIntent.putExtra("buffering", state);
+        sendBroadcast(bufferIntent);
     }
 
 
@@ -150,16 +237,18 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
     @Override
     public void onPrepared(MediaPlayer mp) {
         preparded = true;
+        sendBufferingBroadcast("0");
     }
 
     @Override
     public void onSeekComplete(MediaPlayer mp) {
-
+        sendBufferingBroadcast("4");
     }
 
     @Override
     public void onCompletion(MediaPlayer mp) {
-        //goto next?
+        sendBufferingBroadcast("2");
+        sendBufferingBroadcast("3");
     }
 
     public class MyLocalBinder extends Binder {
--- app/src/main/java/de/boe_dev/spotifystreamer/PlayerActivity.java
+++ app/src/main/java/de/boe_dev/spotifystreamer/PlayerActivity.java
@@ -3,9 +3,12 @@ package de.boe_dev.spotifystreamer;
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Build;
 import android.os.Bundle;
@@ -42,11 +45,13 @@ public class PlayerActivity extends Activity {
 
     private ArrayList<TopTrackModel> list;
     private int pos;
-    private Thread seekBarThread;
-
-    private SimpleDateFormat df = new SimpleDateFormat();
-    private MediaPlayerService mediaPlayerService;
     private static boolean isBound = false;
+    private boolean mBufferBroadcasRegistered;
+    private int playTime = 0;
+
+    private MediaPlayerService mediaPlayerService;
+    private ProgressDialog progressBuffer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,22 +71,40 @@ public class PlayerActivity extends Activity {
     }
 
     @Override
-    protected void onStop() {
-        if (mediaPlayerService.isPlaying() && Build.VERSION.SDK_INT >= 21) {
-            Notification notification = new Notification.Builder(this)
-                    .setVisibility(Notification.VISIBILITY_PUBLIC)
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .addAction(android.R.drawable.ic_media_previous, "Previous", null) //TODO change this
-                    .addAction(android.R.drawable.ic_media_pause, "Pause", null) //TODO change this
-                    .addAction(android.R.drawable.ic_media_next, "Next", null) //TODO change this
-                    .setStyle(new Notification.MediaStyle())
-                    .setContentTitle(list.get(pos).getName())
-                    .setContentText(list.get(pos).getArtist() + "\n" + list.get(pos).getAlbum())
-                    .build();
-
-            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            notificationManager.notify(0, notification);
+    protected void onResume() {
+        if (!mBufferBroadcasRegistered) {
+            registerReceiver(broadcastBufferReceiver, new IntentFilter(MediaPlayerService.BUFFER));
+            mBufferBroadcasRegistered = true;
         }
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mBufferBroadcasRegistered) {
+            unregisterReceiver(broadcastBufferReceiver);
+            mBufferBroadcasRegistered = false;
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+//        if (mediaPlayerService.isPlaying() && Build.VERSION.SDK_INT >= 21) {
+//            Notification notification = new Notification.Builder(this)
+//                    .setVisibility(Notification.VISIBILITY_PUBLIC)
+//                    .setSmallIcon(R.mipmap.ic_launcher)
+//                    .addAction(android.R.drawable.ic_media_previous, "Previous", null) //TODO change this
+//                    .addAction(android.R.drawable.ic_media_pause, "Pause", null) //TODO change this
+//                    .addAction(android.R.drawable.ic_media_next, "Next", null) //TODO change this
+//                    .setStyle(new Notification.MediaStyle())
+//                    .setContentTitle(list.get(pos).getName())
+//                    .setContentText(list.get(pos).getArtist() + "\n" + list.get(pos).getAlbum())
+//                    .build();
+//
+//            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//            notificationManager.notify(0, notification);
+//        }
 
         if (isBound) {
             getApplicationContext().unbindService(playerConnection);
@@ -130,7 +153,12 @@ public class PlayerActivity extends Activity {
     }
 
     private void setupPlayer() {
-        mediaPlayerService.setupPlayer(list, pos);
+        new Thread() {
+            @Override
+            public void run() {
+                mediaPlayerService.setupPlayer(list, pos);
+            }
+        }.start();
     }
 
 
@@ -151,10 +179,14 @@ public class PlayerActivity extends Activity {
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayerService.setPosition(seekBar.getProgress());
+                }
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
+
             }
 
             @Override
@@ -166,69 +198,67 @@ public class PlayerActivity extends Activity {
         play.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
-//                if (isBound && isChecked) {
-//                    mediaPlayerService.loadPlayer();
-//                }
-
                 if (isChecked) {
                     mediaPlayerService.play();
                     seekBarAction();
                 } else {
                     mediaPlayerService.pause();
-
-
                 }
-
             }
         });
 
         prev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mediaPlayerService.previous(play.isChecked());
-                fillView();
-                if (play.isChecked()) {
-                    seekBarAction();
-                }
 
-//                play.setSelected(false);
+                new Thread() {
+                    @Override
+                    public void run() {
+                        mediaPlayerService.previous(play.isChecked());
+                        if (play.isChecked()) {
+                            seekBarAction();
+                        }
+                    }
+                }.start();
             }
         });
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mediaPlayerService.next(play.isChecked());
-                fillView();
-                if (play.isChecked()) {
-                    seekBarAction();
-                }
-                // play.setSelected(false);
 
+                new Thread() {
+                    @Override
+                    public void run() {
+                        mediaPlayerService.next(play.isChecked());
+                        if (play.isChecked()) {
+                            seekBarAction();
+                        }
+                    }
+                }.start();
             }
         });
 
     }
 
     private void fillView() {
-        artist.setText(mediaPlayerService.getArtist());
-        album.setText(mediaPlayerService.getAlbum());
-        track.setText(mediaPlayerService.getName());
-
-        if (!mediaPlayerService.getImageUrl().equals("")) {
-            Picasso.with(this).load(mediaPlayerService.getImageUrl()).into(cover);
-        } else {
-            Picasso.with(this).load(R.drawable.image_not_found).into(cover);
+        if (mediaPlayerService != null) {
+            artist.setText(mediaPlayerService.getArtist());
+            album.setText(mediaPlayerService.getAlbum());
+            track.setText(mediaPlayerService.getName());
+            seekBar.setProgress(0);
+            if (!mediaPlayerService.getImageUrl().equals("")) {
+                Picasso.with(this).load(mediaPlayerService.getImageUrl()).into(cover);
+            } else {
+                Picasso.with(this).load(R.drawable.image_not_found).into(cover);
+            }
         }
-
-        seekBar.setProgress(0);
     }
 
     private void seekBarAction() {
         new Thread() {
             public void run() {
-                while (mediaPlayerService.isPreparded() && mediaPlayerService.getCurrentPosition() != mediaPlayerService.getDuration()) {
+                while (mediaPlayerService.isPlaying() && mediaPlayerService.getCurrentPosition() != mediaPlayerService.getDuration()) {
                     try {
                         runOnUiThread(new Runnable() {
                             @Override
@@ -267,10 +297,8 @@ public class PlayerActivity extends Activity {
             mediaPlayerService = binder.getService();
             isBound = true;
             setupPlayer();
-            seekBar.setMax(mediaPlayerService.getDuration());
-            playedTime.setText(secondsToString((mediaPlayerService.getCurrentPosition() / 1000)));
-            compleatTime.setText(secondsToString((mediaPlayerService.getDuration() / 1000)));
-            fillView();
+
+
         }
 
         @Override
@@ -279,4 +307,51 @@ public class PlayerActivity extends Activity {
             isBound = false;
         }
     };
+
+    private void showProgressDialog(Intent bufferIntent) {
+
+        String bufferValue = bufferIntent.getStringExtra("buffering");
+        int bufferIntValue = Integer.parseInt(bufferValue);
+
+        switch (bufferIntValue) {
+            case 0:
+                if (progressBuffer != null) {
+                    fillView();
+                    seekBar.setMax(mediaPlayerService.getDuration());
+                    playedTime.setText(secondsToString((mediaPlayerService.getCurrentPosition() / 1000)));
+                    playTime = (mediaPlayerService.getDuration() / 1000);
+                    compleatTime.setText(secondsToString(playTime));
+                    progressBuffer.dismiss();
+                }
+                break;
+
+            case 1:
+                progressBuffer = ProgressDialog.show(PlayerActivity.this, "Buffering...", "Data will load...", true);
+                break;
+
+            case 2:
+                play.setChecked(false);
+                break;
+
+            case 3:
+                seekBar.setProgress(0);
+                playedTime.setText("00:00");
+                break;
+
+            case 4:
+                playedTime.setText(secondsToString((mediaPlayerService.getCurrentPosition() / 1000)));
+                break;
+        }
+
+    }
+
+    private BroadcastReceiver broadcastBufferReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            showProgressDialog(intent);
+        }
+    };
+
+
+
 }
